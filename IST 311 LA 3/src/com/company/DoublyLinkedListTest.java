d1f673b1dd960ea2af91b61930a54db3db172d98
Mona Fan <wjf5195@psu.edu>
2022-11-09 23:33:20 -0500

    test inset function complete

--- IST 311 LA 3/src/com/company/DoublyLinkedListTest.java
+++ IST 311 LA 3/src/com/company/DoublyLinkedListTest.java
@@ -50,10 +50,30 @@ class DoublyLinkedListTest {
 
     @Test
     void testInsert() {
-
-
-
-    }
+            DoublyLinkedList dl = new DoublyLinkedList();
+            ArrayList<String> list = new ArrayList<>();
+            list.add("haha");
+            list.add("sjoidj sss");
+            Album a1 = new Album(1,list,"a",1);
+            Album a2 = new Album(2,list,"b",3);
+            Album a3 = new Album(3,list,"c",6);
+            Album a4 = new Album(4,list,"d",7);
+            //insert when the list is empty
+            assertEquals(a1,dl.insert(0,a1).album);
+            System.out.println(dl.toString());
+            //insert at head
+            assertEquals(a2,dl.insert(0,a2).album);
+            System.out.println(dl.toString());
+            //insert at tail
+            assertEquals(a3,dl.insert(2,a3).album);
+            System.out.println(dl.toString());
+            //insert at middle
+            assertEquals(a4,dl.insert(1,a4).album);
+            System.out.println(dl.toString());
+            //when the insert position is out of bounce
+            dl.insert(5,a1);
+        }
+        
 
 
     @Test
